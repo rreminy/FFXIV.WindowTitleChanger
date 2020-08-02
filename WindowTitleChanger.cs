db56bcd876e45a8c13adf2b03790fd62595beeb2
AzureGem <rreminy@gmail.com>
2020-08-01 22:00:30 -0400

    Initial Commit

---
+++ WindowTitleChanger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Dalamud.Plugin;
+using System.Diagnostics;
+
+namespace WindowTitleChanger
+{
+    public class WindowTitleChanger : IDalamudPlugin
+    {
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+
+        public string Name => "Window Title Changer";
+        private string OriginalTitle { get; set; }
+        private DalamudPluginInterface PluginInterface { get; set; }
+        private IntPtr Handle { get; set; }
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.PluginInterface = pluginInterface;
+
+            Process process = Process.GetCurrentProcess();
+            this.Handle = process.MainWindowHandle;
+            this.OriginalTitle = process.MainWindowTitle;
+
+            pluginInterface.CommandManager.AddHandler("/wintitle", new Dalamud.Game.Command.CommandInfo((string command, string arguments) =>
+            {
+                if (string.IsNullOrWhiteSpace(arguments)) arguments = this.OriginalTitle;
+                try { SetWindowText(this.Handle, arguments.Trim()); } catch (Exception e) { PluginLog.LogError($"{e}"); }
+            }));
+
+            process.Dispose();
+        }
+
+        public void Dispose()
+        {
+            try { SetWindowText(this.Handle, this.OriginalTitle); } catch (Exception e) { PluginLog.LogError($"{e}"); }
+            this.PluginInterface.CommandManager.RemoveHandler("/wintitle");
+            this.PluginInterface.Dispose();
+        }
+    }
+}
